de1fc80cac8ec3beaf236550e13bf1fac1f7c265
Sammi Beard <112604652+Slbeard@users.noreply.github.com>
2024-12-22 22:08:58 -0600

    Update recomend_app.py

--- recomend_app.py
+++ recomend_app.py
@@ -103,7 +103,7 @@ if recommended_jobs.empty:
     st.write("No matching jobs found.")
 else:
     for _, job in recommended_jobs.iterrows():
-        st.write(f"**Job Title:** {job['jobtitle']}")
+        st.write(f"**Job Title:** {job['job_title']}")
         st.write(f"**Company:** {job['company']}")
         st.write(f"**Average Salary:** ${job['avg_salary']:,.2f}")
         st.write("---")
